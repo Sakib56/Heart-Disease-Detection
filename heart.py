bc38c2548a7660fb81dcb3fed4fa6c286f2e65e1
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-24 10:32:46 +0100

    functions added, crossvalidation added

--- heart.py
+++ heart.py
@@ -1,32 +1,54 @@
 import numpy as np
 from sklearn import svm
+from random import shuffle
 
-# load data
-Xs = []
-Ys = []
-with open("heartdataset.csv") as dataCSV:
-    for i, row in enumerate(dataCSV):
-        if (i > 0):
-            arrayRow = map(lambda x: x.strip(), row.split(","))
-            cleanedRow = list(map(lambda y: float(y), arrayRow))
 
-            Xs.append(cleanedRow[:-1])
-            Ys.append(cleanedRow[-1])
-Xs = np.array(Xs)
-Ys = np.array(Ys)
+def loadData(fileName):
+    dataTups = []
+    with open(fileName) as dataCSV:
+        for i, row in enumerate(dataCSV):
+            if (i > 0):
+                rowArray = map(lambda x: x.strip(), row.split(","))
+                cleanedRow = list(map(lambda y: float(y), rowArray))
+                dataTups.append((cleanedRow[:-1], cleanedRow[-1]))
+    return dataTups
 
-# init svm
-classifier = svm.SVC(gamma='auto')
-i = int(4*len(Xs)/5)
-classifier.fit(Xs[:i], Ys[:i])
+
+def crossValidate(dataTup, trainSize=0.8):
+    shuffle(dataTup)
+
+    size = int(len(dataTup)*trainSize)
+    trainingDataTup = dataTup[:size]
+    testingDataTup = dataTup[size:]
+
+    trainingData = []
+    trainingTargets = []
+    for data, targets in trainingDataTup:
+        trainingData.append(data)
+        trainingTargets.append(targets)
+
+    testingData = []
+    testingTargets = []
+    for data, targets in testingDataTup:
+        testingData.append(data)
+        testingTargets.append(targets)
+
+    return np.array(trainingData), np.array(trainingTargets), np.array(testingData), np.array(testingTargets)
+
+
+dataTup = loadData("heartdataset.csv")
+trainingData, trainingTargets, testingData, testingTargets = crossValidate(dataTup)
+
+classifier = svm.SVC(gamma=0.0001, C=100)
+classifier.fit(trainingData, trainingTargets)
 
 total = 0
-correct = 0 
-for X,Y in zip(Xs[i:], Ys[i:]):
+correct = 0
+for X, Y in zip(testingData, testingTargets):
     if int(classifier.predict([X])) == Y:
         correct += 1
     total += 1
 print(correct/total)
 
 # print("data: {0}\ntarget: {1}\npred: {2}".format(
-#     Xs[i], Ys[i], classifier.predict([Xs[i]])))
+#     data[i], targets[i], classifier.predict([data[i]])))
