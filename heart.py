50c37ac4cce23f40504d826be96a24acef3b6a4b
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-25 13:16:56 +0100

    comments added

--- heart.py
+++ heart.py
@@ -2,7 +2,8 @@ import numpy as np
 from sklearn import svm
 from random import shuffle
 
-
+# loads the data from "heartdataset.csv" and converts
+# rows to floats tuples of (data, target)
 def loadData(fileName):
     dataTups = []
     with open(fileName) as dataCSV:
@@ -13,7 +14,8 @@ def loadData(fileName):
                 dataTups.append((cleanedRow[:-1], cleanedRow[-1]))
     return dataTups
 
-
+# randomises order of data, splitting data into
+# a training and testing set
 def crossValidate(dataTup, trainSize=0.8):
     shuffle(dataTup)
 
@@ -39,9 +41,11 @@ def crossValidate(dataTup, trainSize=0.8):
 dataTup = loadData("heartdataset.csv")
 trainingData, trainingTargets, testingData, testingTargets = crossValidate(dataTup)
 
+# init of svm (classifier) and "trains" with training set
 classifier = svm.SVC(gamma=0.000043, C=700)
 classifier.fit(trainingData, trainingTargets)
 
+# calculates accuracy using testing set
 total = 0
 correct = 0
 for X, Y in zip(testingData, testingTargets):
@@ -50,5 +54,6 @@ for X, Y in zip(testingData, testingTargets):
     total += 1
 print(correct/total)
 
-#g = 4.274e-05
-#c = 700
+# from brute force testing, best gamma, g and constant, c are...
+# g = 4.274e-05
+# c = 700
